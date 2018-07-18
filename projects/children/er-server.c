5d0dc501e41abc90455ab7bc1d38e931c667aefa
yoyo890121 <yoyo890121@hotmail.com>
2018-07-18 15:46:06 +0800

    experiment

--- projects/children/er-server.c
+++ projects/children/er-server.c
@@ -132,6 +132,16 @@ PROCESS_THREAD(node_process, ev, data)
   static struct etimer etaa;
   PROCESS_BEGIN();
 
+  etimer_set(&etaa, CLOCK_SECOND * 600);
+  PROCESS_YIELD_UNTIL(etimer_expired(&etaa));
+  etimer_reset(&etaa);
+
+#if CONTIKI_TARGET_COOJA
+  extern uint8_t event_threshold;
+  event_threshold = 20;
+  printf("set event_threshold=%d\n", event_threshold);
+#endif /* CONTIKI_TARGET_COOJA */
+
   etimer_set(&etaa, CLOCK_SECOND * 3600);
   PROCESS_YIELD_UNTIL(etimer_expired(&etaa));
   etimer_reset(&etaa);
--- projects/children/resources/res-bcollect.c
+++ projects/children/resources/res-bcollect.c
@@ -43,7 +43,7 @@ PERIODIC_RESOURCE(res_bcollect,
 static uint32_t event_counter = 0;
 
 /* inter-packet time we generate a packet to send to observer */
-uint8_t event_threshold = 20;
+uint8_t event_threshold = 240;
 
 /* record last change event threshold's event_counter */
 static uint32_t event_threshold_last_change = 0;
